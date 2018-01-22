df904206f2c717c1b8c1d3b51d5e753c75e5a2a9
Shreyash Pandey <shreyashpandey@DN51t44k.SUNet>
2018-01-22 01:02:46 -0800

    Preprocessed data to JSON

--- clean_data.py
+++ clean_data.py
@@ -1,3 +1,5 @@
+import json
+
 raw_data = open("final_data_2.0.txt").read().split("\n")[:-1]
 print "Length of Raw data: " , len(raw_data)
 
@@ -6,7 +8,7 @@ print "Length of data: " , len(data)
 
 
 
-COND_OPS = ['=', '>', '<','<>','<=', '>=']
+COND_OPS = ['EQL', 'GT', 'LT','NEQL','LTEQL', 'GTEQL']
 REL_OPS = ['AND']
 
 def count_cond(q):
@@ -16,7 +18,7 @@ def count_cond(q):
     return c + 1
 
 def check_validity(q):
-    if 'where' not in q:
+    if 'WHERE' not in q:
         return False
     if 'OR' in q:
         return False
@@ -25,10 +27,70 @@ def check_validity(q):
             return True
     return False
 
-clean_data = filter(lambda x: len(x.split('|')[1].strip().split()) != 0 and count_cond(x.split('|')[2].strip().split()) < 4 and check_validity(x.split('|')[2].strip().split()), data)
+def check_num_pipes(q):
+    if q.count('|') == 3:
+        return True
+    else:
+        return False
+
+mod_data   = map(lambda x: x.replace('where', 'WHERE').replace('(','').replace(')','').\
+                    replace('<>','NEQL').replace('<=','LTEQL').replace('>=','GTEQL').\
+                    replace('<','LT').replace('>','GT').replace('=','EQL').strip(), data)
+clean_data = filter(lambda x: len(x.split('|')[1].strip().split()) != 0 and \
+                    count_cond(x.split('|')[2].strip().split()) < 4 and \
+                    check_validity(x.split('|')[2].strip().split()) and check_num_pipes(x), mod_data)
 print "Length of Clean data: " , len(clean_data)
 
-small_data = clean_data[:25000]
-out_file = open("small_data.txt","w")
+small_data = clean_data[:50000]
+out_file = open("clean_data.txt","w")
 for d in small_data:
     out_file.write(d+"\n")
+
+
+json_data = []
+for d in clean_data:
+    sql = {}
+    query_tok = d.split('|')[2].strip().split()
+    query = ' '.join(query_tok)
+    if len(d.split('|')[1].strip().split()) == 0 or \
+        count_cond(d.split('|')[2].strip().split()) >= 4 or 'NEQLEQL' in query:
+        continue
+    cols = [' '.join(x.strip().split()) for x in d.split('|')[3].strip().split(',')]
+    gt = []    
+    try:
+        start_ind = query_tok.index('WHERE') + 1
+        curr_ind = start_ind + 1
+        while (curr_ind < len(query_tok)):
+            if query_tok[curr_ind] in COND_OPS:
+                col = cols.index(' '.join(query_tok[start_ind:curr_ind]).strip())
+                start_ind = curr_ind + 1
+                op = COND_OPS.index(query_tok[curr_ind])
+            if query_tok[curr_ind] in REL_OPS:
+                val = ' '.join(query_tok[start_ind:curr_ind]).strip()
+                start_ind = curr_ind + 1
+                gt.append([col,op,val])            
+            curr_ind += 1
+        
+        val = ' '.join(query_tok[start_ind:curr_ind]).strip()
+        gt.append([col,op,val]) 
+    except:
+        continue
+   
+    if len(gt) >= 4:
+        continue
+   
+    sql['query'] = query
+    sql['query_tok'] = query_tok
+    sql['schema'] = cols
+    sql['NL']   = d.split('|')[1].strip()
+    sql['NL_tok'] = d.split('|')[1].strip().split()
+    sql['schema_tok'] = [x.strip().split() for x in cols]
+    sql['sql'] = {'agg':None, 'sel':None}
+    sql['sql']['conds'] = gt
+
+    json_data.append(sql)
+
+print len(json_data)
+
+with open('constraint_data.json', 'w') as outfile:
+    json.dump(json_data, outfile)  
